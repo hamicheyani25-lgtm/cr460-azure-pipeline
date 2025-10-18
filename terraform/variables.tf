34cfb5d129ae33229bc997e8525d84c6544c1e98
hamicheyani25-lgtm <hamiche.yani25@gmail.com>
2025-10-18 17:15:26 -0400

    Add variables for Azure region and resource prefix

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+# Variables placeholders (on remplira à Q7+)
+variable "location" {
+  type        = string
+  default     = "canadacentral"
+  description = "Azure region"
+}
+
+variable "prefix" {
+  type        = string
+  default     = "cr460-yani"
+  description = "Prefix for resource names"
+}
